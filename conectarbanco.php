ba755a963607447dd1ed6747c30345b3a03ecebf
Lucas.FR <62547642+LucasFR07@users.noreply.github.com>
2024-04-10 17:24:17 -0300

    conectarbanco.php

--- conectarbanco.php
+++ conectarbanco.php
@@ -1,9 +1,9 @@
 <?php
 
 $config = array(
-    'db_user' => 'trade021_subwaysurf',
-    'db_pass' => '+c!{3{5u)]_[',
-    'db_name' => 'trade021_subwaysurf',
+    'db_user' => 'trade_subwaysurf',
+    'db_pass' => '+c!h{3&{5u)+#]_[',
+    'db_name' => 'trade_subwaysurf',
     'db_host' => 'localhost'
 );
 
